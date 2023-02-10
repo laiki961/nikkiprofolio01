fd8edc464677143c3e91351de804ee7deade2090
Nikki <laiki961@gmail.com>
2023-02-10 13:49:42 -0500

    init backend

---
+++ backend/portfolio-spingboot-01/src/main/java/com/nikki/portfoliospingboot01/PortfolioSpingboot01Application.java
@@ -0,0 +1,13 @@
+package com.nikki.portfoliospingboot01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioSpingboot01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioSpingboot01Application.class, args);
+	}
+
+}
---
+++ backend/portfolio-spingboot-01/src/test/java/com/nikki/portfoliospingboot01/PortfolioSpingboot01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikki.portfoliospingboot01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioSpingboot01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
