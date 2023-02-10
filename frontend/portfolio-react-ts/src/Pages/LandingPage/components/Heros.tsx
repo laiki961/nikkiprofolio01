9e1e7404241d4a14bd30629f5f92a185c9310a9e
Nikki <laiki961@gmail.com>
2023-02-10 18:34:17 -0500

    frontend - reordered the Heros component

--- frontend/portfolio-react-ts/src/Pages/LandingPage/components/Heros.tsx
+++ frontend/portfolio-react-ts/src/Pages/LandingPage/components/Heros.tsx
@@ -8,14 +8,17 @@ export const Heros = () => {
   return (
     <div>
       <div className='d-none d-lg-block'>
-        <div className='row g-0 mt-5'>
-          <div className='col-sm-6 col-md-6'>
-            <div className='col-image-left'></div>
-            <div className={classes["col-image-left"]}></div>
-          </div>
-          <div className='col-4 col-md-4 container d-flex justify-content-center align-items-center'>
+        {/* ADD START*/}
+        <div className='row g-0'>
+          <div
+            className='col-4 col-md-4 container d-flex 
+                        justify-content-center align-items-center'
+          >
             <div className='ml-2'>
-              <h1>eCommerce</h1>
+              <h1>
+                Weather Forecast{" "}
+                <i className={classes["text-third"]}>by Open Weather</i>
+              </h1>
               <p className='lead'>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
                 tincidunt, leo eu vestibulum convallis, ante enim convallis
@@ -24,8 +27,34 @@ export const Heros = () => {
                 tristique dolor augue ut mauris. Quisque at varius mauris, vitae
                 maximus velit. Donec in aliquam.
               </p>
-              <Link className='btn btn btn-primary btn-lg' to='/ecommerce'>
-                Sign up
+              <Link className='btn btn-dark btn-lg' to='/weather'>
+                Check it out
+              </Link>
+            </div>
+          </div>
+          <div className='col-sm-6 col-md-6'>
+            <div className={classes["col-image-weather"]}></div>
+          </div>
+        </div>
+        {/* ADD END*/}
+        <div className='row g-0'>
+          <div className='col-sm-6 col-md-6'>
+            <div className='col-image-left'></div>
+            <div className={classes["col-image-library"]}></div>
+          </div>
+          <div className='col-4 col-md-4 container d-flex justify-content-center align-items-center'>
+            <div className='ml-2'>
+              <h1>Library</h1>
+              <p className='lead'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                tincidunt, leo eu vestibulum convallis, ante enim convallis
+                libero, et sodales arcu ipsum id nisl. Maecenas consequat, lacus
+                ut ultricies hendrerit, orci mauris imperdiet nibh, scelerisque
+                tristique dolor augue ut mauris. Quisque at varius mauris, vitae
+                maximus velit. Donec in aliquam.
+              </p>
+              <Link className='btn btn-dark btn-lg' to='/library'>
+                Check it out
               </Link>
             </div>
           </div>
@@ -36,21 +65,22 @@ export const Heros = () => {
                         justify-content-center align-items-center'
           >
             <div className='ml-2'>
-              <h1>Library</h1>
+              <h1>eCommerce</h1>
               <p className='lead'>
-                Try to check in daily as our collection is always changing! We
-                work nonstop to provide the most accurate book selection
-                possible for our Luv 2 Read students! We are diligent about our
-                book selection and our books are always going to be our top
-                priority.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                tincidunt, leo eu vestibulum convallis, ante enim convallis
+                libero, et sodales arcu ipsum id nisl. Maecenas consequat, lacus
+                ut ultricies hendrerit, orci mauris imperdiet nibh, scelerisque
+                tristique dolor augue ut mauris. Quisque at varius mauris, vitae
+                maximus velit. Donec in aliquam.
               </p>
-              <Link className='btn btn btn-primary btn-lg' to='/library'>
-                Sign up
+              <Link className='btn btn-dark btn-lg' to='/ecommerce'>
+                Check it out
               </Link>
             </div>
           </div>
           <div className='col-sm-6 col-md-6'>
-            <div className={classes["col-image-right"]}></div>
+            <div className={classes["col-image-ecomerce"]}></div>
           </div>
         </div>
       </div>
@@ -59,9 +89,28 @@ export const Heros = () => {
       <div className='d-lg-none'>
         <div className='container'>
           <div className='m-2'>
-            <div className={classes["col-image-left"]}></div>
+            <div className={classes["col-image-weather"]}></div>
             <div className='mt-2'>
-              <h1>eCommerce</h1>
+              <h1>
+                Weather Forecast{" "}
+                <i className={classes["text-third"]}>by Open Weather</i>
+              </h1>
+              <p className='lead'>
+                Try to check in daily as our collection is always changing! We
+                work nonstop to provide the most accurate book selection
+                possible for our Luv 2 Read students! We are diligent about our
+                book selection and our books are always going to be our top
+                priority.
+              </p>
+              <Link className='btn btn-dark btn-md' to='/weather'>
+                Check it out
+              </Link>
+            </div>
+          </div>
+          <div className='m-2'>
+            <div className={classes["col-image-library"]}></div>
+            <div className='mt-2'>
+              <h1>Library</h1>
               <p className='lead'>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
                 tincidunt, leo eu vestibulum convallis, ante enim convallis
@@ -72,17 +121,17 @@ export const Heros = () => {
               </p>
               <Link
                 type='button'
-                className='btn btn-primary btn-md'
+                className='btn btn-dark btn-md'
                 to='/ecommerce'
               >
-                Sign up
+                Check it out
               </Link>
             </div>
           </div>
           <div className='m-2'>
-            <div className={classes["col-image-right"]}></div>
+            <div className={classes["col-image-library"]}></div>
             <div className='mt-2'>
-              <h1>Library</h1>
+              <h1>eCommerce</h1>
               <p className='lead'>
                 Try to check in daily as our collection is always changing! We
                 work nonstop to provide the most accurate book selection
@@ -90,8 +139,8 @@ export const Heros = () => {
                 book selection and our books are always going to be our top
                 priority.
               </p>
-              <Link className='btn btn-primary btn-md' to='/library'>
-                Sign up
+              <Link className='btn btn-dark btn-md' to='/ecommerce'>
+                Check it out
               </Link>
             </div>
           </div>
