9785cc3111fd86ba1e0af2d660464ab57c02a249
Nikki <laiki961@gmail.com>
2023-02-10 21:12:40 -0500

    backend - Create Config and Added Book Entity

--- backend/portfolio-spingboot-01/src/main/java/com/nikki/portfoliospingboot01/entity/Book.java
+++ backend/portfolio-spingboot-01/src/main/java/com/nikki/portfoliospingboot01/entity/Book.java
@@ -1,2 +1,35 @@
-package com.nikki.portfoliospingboot01.entity;public class Book {
+package com.nikki.portfoliospingboot01.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "book")
+@Data
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "copies")
+    private int copies;
+
+    @Column(name = "copies_available")
+    private int copiesAvailable;
+
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "img")
+    private String img;
 }
