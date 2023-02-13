f23d76a1536b11e574cbedacdcc230f95fd4de71
Nikki <laiki961@gmail.com>
2023-02-13 17:35:00 -0500

    Backend - Added BookController

--- backend/portfolio-spingboot-01/src/main/java/com/nikki/portfoliospingboot01/controller/BookController.java
+++ backend/portfolio-spingboot-01/src/main/java/com/nikki/portfoliospingboot01/controller/BookController.java
@@ -1,10 +1,9 @@
 package com.nikki.portfoliospingboot01.controller;
 
+import com.nikki.portfoliospingboot01.entity.Book;
 import com.nikki.portfoliospingboot01.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin("http://localhost:3000")
 @RestController
@@ -15,5 +14,11 @@ public class BookController {
     @Autowired
     public BookController(BookService bookService) {
         this.bookService = bookService;
+    }
+
+    @PutMapping("/secure/checkout")
+    public Book checkout (@RequestParam Long bookId) throws Exception {
+        String userEmail = "testuser@email.com";
+        return bookService.checkoutBook(userEmail, bookId);
     }
 }
