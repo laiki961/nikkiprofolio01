fa2d7b5e09561d1bf72a49211620c468295cb579
Nikki <laiki961@gmail.com>
2023-02-26 14:18:41 -0500

    backend - added increaseBookQuantity in Service & an endpoint increaseBookQuantity in AdminController

--- backend/portfolio-library/src/main/java/com/nikkiportfolio/libraryapp/controller/AdminController.java
+++ backend/portfolio-library/src/main/java/com/nikkiportfolio/libraryapp/controller/AdminController.java
@@ -26,4 +26,14 @@ public class AdminController {
         }
         adminService.postBook(addBookRequest);
     }
+
+    @PutMapping("/secure/increase/book/quantity")
+    public void increaseBookQuantity(@RequestHeader(value="Authorization") String token,
+                                     @RequestParam Long bookId) throws Exception {
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")) {
+            throw new Exception("Administration page only");
+        }
+        adminService.increaseBookQuantity(bookId);
+    }
 }
