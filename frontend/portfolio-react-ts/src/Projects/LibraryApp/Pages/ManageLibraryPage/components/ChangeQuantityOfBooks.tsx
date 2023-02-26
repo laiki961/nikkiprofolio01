2dd1ad8b1a9d049c5eb7ef0fa642d3824ea7a5d0
Nikki <laiki961@gmail.com>
2023-02-26 13:30:59 -0500

    frontend - created ChangeQuantityOfBooks and applied into ManageLibraryPage

---
+++ frontend/portfolio-react-ts/src/Projects/LibraryApp/Pages/ManageLibraryPage/components/ChangeQuantityOfBooks.tsx
@@ -0,0 +1,112 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import BookModel from "../../../Models/BookModel";
+import Loading from "../../../../../Layouts/components/Loading/Loading";
+import { Pagination } from "../../../Utils/Pagination";
+// import { ChangeQuantityOfBook } from "./ChangeQuantityOfBook";
+
+export const ChangeQuantityOfBooks = () => {
+  const [books, setBooks] = useState<BookModel[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [booksPerPage] = useState(5);
+  const [totalAmountOfBooks, setTotalAmountOfBooks] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const baseUrl: string = `http://localhost:8080/library/api/books?page=${
+        currentPage - 1
+      }&size=${booksPerPage}`;
+
+      const response = await fetch(baseUrl);
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const responseJson = await response.json();
+
+      const responseData = responseJson._embedded.books;
+
+      setTotalAmountOfBooks(responseJson.page.totalElements);
+      setTotalPages(responseJson.page.totalPages);
+
+      const loadedBooks: BookModel[] = [];
+
+      for (const key in responseData) {
+        loadedBooks.push({
+          id: responseData[key].id,
+          title: responseData[key].title,
+          author: responseData[key].author,
+          description: responseData[key].description,
+          copies: responseData[key].copies,
+          copiesAvailable: responseData[key].copiesAvailable,
+          category: responseData[key].category,
+          img: responseData[key].img,
+        });
+      }
+
+      setBooks(loadedBooks);
+      setIsLoading(false);
+    };
+    fetchBooks().catch((error: any) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+  }, [currentPage]);
+
+  const indexOfLastBook: number = currentPage * booksPerPage;
+  const indexOfFirstBook: number = indexOfLastBook - booksPerPage;
+  let lastItem =
+    booksPerPage * currentPage <= totalAmountOfBooks
+      ? booksPerPage * currentPage
+      : totalAmountOfBooks;
+
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (httpError) {
+    return (
+      <div className='container my-5 min-vh-100'>
+        <p>{httpError}</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className='container my-5 min-vh-100'>
+      {totalAmountOfBooks > 0 ? (
+        <>
+          <div className='my-3'>
+            <h3>Number of results: ({totalAmountOfBooks})</h3>
+          </div>
+          <p>
+            {indexOfFirstBook + 1} to {lastItem} of {totalAmountOfBooks} items:
+          </p>
+          {books.map((book) => (
+            <p>Display different quantity of books</p>
+            // <ChangeQuantityOfBook
+            //   book={book}
+            //   key={book.id}
+            //   deleteBook={deleteBook}
+            // />
+          ))}
+        </>
+      ) : (
+        <h5>Add a book before changing quantity</h5>
+      )}
+      {totalPages > 1 && (
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          paginate={paginate}
+        />
+      )}
+    </div>
+  );
+};
