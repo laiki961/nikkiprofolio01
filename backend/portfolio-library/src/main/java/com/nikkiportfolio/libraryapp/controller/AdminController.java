475eca06b7c4d3d0863eb9f18dbb23196ca6115f
Nikki <laiki961@gmail.com>
2023-02-26 12:44:37 -0500

    backend - added AddBookRequest Model, added AdminService, added AdminController

---
+++ backend/portfolio-library/src/main/java/com/nikkiportfolio/libraryapp/controller/AdminController.java
@@ -0,0 +1,29 @@
+package com.nikkiportfolio.libraryapp.controller;
+
+import com.nikkiportfolio.libraryapp.requestmodels.AddBookRequest;
+import com.nikkiportfolio.libraryapp.service.AdminService;
+import com.nikkiportfolio.libraryapp.utils.ExtractJWT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("library/api/admin")
+public class AdminController {
+    private AdminService adminService;
+
+    @Autowired
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+    @PostMapping("/secure/add/book")
+    public void postBook(@RequestHeader(value="Authorization") String token,
+                         @RequestBody AddBookRequest addBookRequest) throws Exception {
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")) {
+            throw new Exception("Administration page only");
+        }
+        adminService.postBook(addBookRequest);
+    }
+}
